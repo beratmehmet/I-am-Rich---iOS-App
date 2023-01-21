e3ed102002e4e87d4a6eb6dc547d34ac70a8a537
Berat Mehmet Topuz <m.topuz@emakina.com.tr>
2023-01-21 21:02:29 +0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Berat_Mehmet_Topuz on 21.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
